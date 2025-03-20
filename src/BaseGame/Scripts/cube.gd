fd130f21ee6d999a8813e25c888c0a1ff2e46888
SaidEminnTNGR <saideminaltintirmik0@gmail.com>
2025-03-21 01:49:24 +0300

    v4

--- src/BaseGame/Scripts/cube.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody3D
-
-func _process(delta: float) -> void:
-	if $RayCast3D.is_colliding():
-		pass
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	GLobalVar.emit_signal('CubeCollising')
